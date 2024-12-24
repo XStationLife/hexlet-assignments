daaa976ae91103cd6bbd3e998b67037ab031f9c8
@hexlet/cli <support@hexlet.io>
2024-12-25 00:41:33 +0300

    submit java-web-ru/validation

--- java-web-ru/validation/src/main/java/exercise/App.java
+++ java-web-ru/validation/src/main/java/exercise/App.java
@@ -37,23 +37,23 @@ public final class App {
         });
 
         app.post("/articles", ctx -> {
-           try {
-               var name = ctx.formParamAsClass("articleName", String.class)
-                       .check(value -> value.length() > 2, "Название не должно быть короче двух символов")
-                       .check(value -> ArticleRepository.search(value).isEmpty(), "Статья с таким названием уже существует")
-                       .get();
-               var content = ctx.formParamAsClass("articleContent", String.class)
-                       .check(value -> value.length() > 10, "Статья должна быть не короче 10 символов")
-                       .get();
-               var article = new Article(name, content);
-               ArticleRepository.save(article);
-               ctx.redirect("/articles");
-           } catch (ValidationException e) {
-               var name = ctx.formParam("name");
-               var content = ctx.formParam("content");
-               var page = new BuildArticlePage(name, content, e.getErrors());
-               ctx.render("articles/build.jte", model("page", page)).status(422);
-           }
+            try {
+                var name = ctx.formParamAsClass("articleName", String.class)
+                        .check(value -> value.length() > 2, "Название не должно быть короче двух символов")
+                        .check(value -> ArticleRepository.search(value).isEmpty(), "Статья с таким названием уже существует")
+                        .get();
+                var content = ctx.formParamAsClass("articleContent", String.class)
+                        .check(value -> value.length() > 10, "Статья должна быть не короче 10 символов")
+                        .get();
+                var article = new Article(name, content);
+                ArticleRepository.save(article);
+                ctx.redirect("/articles");
+            } catch (ValidationException e) {
+                var name = ctx.formParam("name");
+                var content = ctx.formParam("content");
+                var page = new BuildArticlePage(name, content, e.getErrors());
+                ctx.render("articles/build.jte", model("page", page)).status(422);
+            }
         });
         // END
 
