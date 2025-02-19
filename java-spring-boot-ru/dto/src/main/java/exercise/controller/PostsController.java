2d2082bda4a27b037e7d1f6da499ce970561c32a
@hexlet/cli <support@hexlet.io>
2025-02-19 23:35:50 +0300

    submit java-spring-boot-ru/dto

--- java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -2,9 +2,10 @@ package exercise.controller;
 
 import exercise.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 import exercise.model.Post;
@@ -34,6 +35,7 @@ public class PostsController {
     }
 
     @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public PostDTO show(@PathVariable long id) {
 
         var post =  postRepository.findById(id)
